83ae3135b37bfc1cbc92d68c434c645e0b57137d
devAnkitGupta <ankitgupta_@outlook.com>
2023-05-01 11:55:45 +0530

    add user model

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,5 @@
 export default {
     port: 1337,
-    dbUri: "mongodb+srv://ankit:test1234@cluster0.hi6oxvw.mongodb.net/?retryWrites=true&w=majority"
+    dbUri: "mongodb+srv://ankit:test1234@cluster0.hi6oxvw.mongodb.net/?retryWrites=true&w=majority",
+    saltWorkFactor: 10,
 }
